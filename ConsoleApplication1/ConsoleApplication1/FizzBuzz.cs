1914324e311b8cfb596db803ccfc09478b2add7b
martin mittermaier <martin.mittermaier@rohde-shwarz.com>
2014-05-16 14:40:38 +0200

    initial

---
+++ ConsoleApplication1/ConsoleApplication1/FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class FizzBuzz
+    {
+        public string FizzBuzzOrNumber(int number)
+        {
+            if (IsDivisibleThree(number) && IsDivisibleByFive(number))
+            {
+                return "FIZZBUZZ";
+            }
+            else if (IsDivisibleThree(number))
+            {
+                return "FIZZ";
+            }
+            else if (IsDivisibleByFive(number))
+            {
+                return "BUZZ";
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
+        
+        public static bool IsDivisibleByFive(int number)
+        {
+            return (number % 5) == 0;
+        }
+
+        public static bool IsDivisibleThree(int number)
+        {
+            return (number % 3) == 0;
+        }
+    }
+
+}
